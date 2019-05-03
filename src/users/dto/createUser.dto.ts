2cda288241dc0b2891803a68f54ae1653074e479
Leonardo Ribeiro <contato@leoribeiroweb.com.br>
2019-05-03 05:36:43 -0300

    docs(swagger): added swagger api documentation

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,13 +1,18 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiModelProperty({ required: true })
   @IsNotEmpty()
   name: string;
 
+  @ApiModelProperty({ required: true })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiModelProperty({ required: true, minLength: 8 })
   @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -7,17 +7,26 @@ import {
   UnauthorizedException,
   UseGuards,
   Req,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { MongoIdValidation } from '../helpers/mongoIdValidation';
 import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
+import { ApiUseTags, ApiBearerAuth, ApiCreatedResponse, ApiUnauthorizedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiUseTags('Wallets')
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ title: 'Create one wallet' })
+  @ApiCreatedResponse({ description: 'Successfull create card'})
+  @ApiUnauthorizedResponse({ description: 'User unauthorized'})
   async create(@Req() req) {
     const userId = req.user._id;
 
@@ -26,6 +35,9 @@ export class WalletsController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
+  @ApiOperation({ title: 'Get wallets' })
+  @ApiUnauthorizedResponse({ description: 'User unauthorized'})
+  @ApiOkResponse({ description: 'Retrive all user wallets' })
   async getWallets(@Req() req) {
     const userId = req.user._id;
 
@@ -34,6 +46,9 @@ export class WalletsController {
 
   @UseGuards(JwtAuthGuard)
   @Get('/:id')
+  @ApiOperation({ title: 'Get one wallet by id' })
+  @ApiUnauthorizedResponse({ description: 'User unauthorized'})
+  @ApiOkResponse({ description: 'Retrive wallet by id' })
   async getWalletById(@Param() params: MongoIdValidation, @Req() req) {
     const userId = req.user._id;
 
